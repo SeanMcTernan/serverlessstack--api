99912ab4d2724066a314117f216cf8abda42f841
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2020-05-27 15:27:13 -0600

    Adding a timeout to test error logging

--- get.js
+++ get.js
@@ -1,18 +1,26 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
+
 export const main = handler(async (event, context) => {
-    const params = {
-        TableName: process.env.tableName,
-        Key: {
-            userId: event.requestContext.identity.cognitoIdentityId,
-            // noteId: event.pathParameters.id
-        }
-    };
-
-    const result = await dynamoDb.get(params);
-    if ( ! result.Item) {
-        throw new Error("Item not found.");
+  const params = {
+    TableName: process.env.tableName,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
     }
+  };
 
-    return result.Item;
+  const result = await dynamoDb.get(params);
+  if ( ! result.Item) {
+    throw new Error("Item not found.");
+  }
+
+  // Set a timeout
+  await new Promise(resolve => setTimeout(resolve, 10000));
+
+  // Return the retrieved item
+  return result.Item;
 });
